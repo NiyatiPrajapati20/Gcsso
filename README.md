7c082855395a98d72eb0aefc525dab7d4e976e45
NiyatiPrajapati20 <82589122+NiyatiPrajapati20@users.noreply.github.com>
2021-05-14 19:15:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Gcsso
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,48 @@
+var car, wall;
+var speed, weight;
+
+ function setup(){
+   createCanvas(1600,400);
+   car = createSprite(200,200,50,50)
+   wall = createSprite(1200,200,60,100);
+
+   car.velocityX = 0  ;
+
+   speed =random(21,35);
+   weight = random(400,1500);
+
+ }
+
+function draw() {
+  background("black");
+
+
+  if(wall.x-car.x<(car.width+wall.width)/2);
+  {
+    car.velocityX = 5;
+    var deformation = 0.5 *weight * speed * speed/22509;
+    if(deformation>180)
+    {
+      car.shapeColor = color("yellow");
+      wall.shapeColor = color("white");
+    }
+    if (deformation<180 && deformation>100)
+    {
+      car.shapeColor = color("green");
+      wall.shapeColor = color("white");
+    }
+    if(deformation<100);
+    {
+      car.shapeColor = color("red");
+      wall.shapeColor = color('white');
+    }
+   }
+
+   drawSprites();
+
+}
+
+
+
+
+
